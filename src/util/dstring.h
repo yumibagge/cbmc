8765b34c392f5971dcdbe56dd1c0861b50181480
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-05-25 11:23:43 +0100

    enable conversion from irep_idt to enum class

--- src/util/dstring.h
+++ src/util/dstring.h
@@ -20,7 +20,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 /// \ref dstringt has one field, an unsigned integer \ref no which is an index
 /// into a static table of strings. This makes it expensive to create a new
-/// string(because you have to look through the whole table to see if it is
+/// string (because you have to look through the whole table to see if it is
 /// already there, and add it if it isn't) but very cheap to compare strings
 /// (just compare the two integers). It also means that when you have lots of
 /// copies of the same string you only have to store the whole string once,
@@ -29,10 +29,7 @@ Author: Daniel Kroening, kroening@kroening.com
 /// `irep_idt` and `irep_namet` are typedef-ed to \ref dstringt in irep.h unless
 /// `USE_STD_STRING` is set.
 ///
-///
-/// Note: Marked final to disable inheritance. No virtual destructor, so
-/// runtime-polymorphic use would be unsafe.
-class dstringt final
+class dstringt
 {
 public:
   // this is safe for static objects
@@ -151,7 +148,7 @@ public:
     return no;
   }
 
-private:
+protected:
   #ifdef __GNUC__
   constexpr
   #endif
