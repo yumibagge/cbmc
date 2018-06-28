869faf52dbaeecbf1d4e86d6dc4dc92d5bddcfd2
Joel Allred <joel.allred@diffblue.com>
2018-06-28 17:45:56 +0100

    Remove dead code: byte_extract_(big|little)_endian
    Constructors for byte_extract_big_endian and byte_extract_little_endian
    are not used.

--- src/util/byte_operators.h
+++ src/util/byte_operators.h
@@ -66,75 +66,9 @@ irep_idt byte_update_id();
 
 /*! \brief TO_BE_DOCUMENTED
 */
-class byte_extract_little_endian_exprt:public byte_extract_exprt
-{
-public:
-  byte_extract_little_endian_exprt():
-    byte_extract_exprt(ID_byte_extract_little_endian)
-  {
-  }
-};
-
-inline const byte_extract_little_endian_exprt
-  &to_byte_extract_little_endian_expr(const exprt &expr)
-{
-  assert(expr.id()==ID_byte_extract_little_endian && expr.operands().size()==2);
-  return static_cast<const byte_extract_little_endian_exprt &>(expr);
-}
-
-inline byte_extract_little_endian_exprt
-  &to_byte_extract_little_endian_expr(exprt &expr)
-{
-  assert(expr.id()==ID_byte_extract_little_endian && expr.operands().size()==2);
-  return static_cast<byte_extract_little_endian_exprt &>(expr);
-}
-
-/*! \brief TO_BE_DOCUMENTED
-*/
-class byte_extract_big_endian_exprt:public byte_extract_exprt
-{
-public:
-  byte_extract_big_endian_exprt():
-    byte_extract_exprt(ID_byte_extract_big_endian)
-  {
-  }
-
-  byte_extract_big_endian_exprt(
-    const exprt &_op, const exprt &_offset, const typet &_type):
-    byte_extract_exprt(ID_byte_extract_big_endian, _op, _offset, _type)
-  {
-  }
-};
-
-inline const byte_extract_big_endian_exprt
-  &to_byte_extract_big_endian_expr(const exprt &expr)
-{
-  assert(expr.id()==ID_byte_extract_big_endian && expr.operands().size()==2);
-  return static_cast<const byte_extract_big_endian_exprt &>(expr);
-}
-
-inline byte_extract_big_endian_exprt
-  &to_byte_extract_big_endian_expr(exprt &expr)
-{
-  assert(expr.id()==ID_byte_extract_big_endian && expr.operands().size()==2);
-  return static_cast<byte_extract_big_endian_exprt &>(expr);
-}
-
-/*! \brief TO_BE_DOCUMENTED
-*/
 class byte_update_exprt:public exprt
 {
 public:
-  explicit byte_update_exprt(irep_idt _id):exprt(_id)
-  {
-    operands().resize(3);
-  }
-
-  byte_update_exprt(irep_idt _id, const typet &_type):
-    exprt(_id, _type)
-  {
-    operands().resize(3);
-  }
 
   byte_update_exprt(
     irep_idt _id,
@@ -163,68 +97,6 @@ inline byte_update_exprt &to_byte_update_expr(exprt &expr)
 {
   assert(expr.operands().size()==3);
   return static_cast<byte_update_exprt &>(expr);
-}
-
-/*! \brief TO_BE_DOCUMENTED
-*/
-class byte_update_little_endian_exprt:public byte_update_exprt
-{
-public:
-  byte_update_little_endian_exprt():
-    byte_update_exprt(ID_byte_update_little_endian)
-  {
-  }
-
-  byte_update_little_endian_exprt(
-    const exprt &_op, const exprt &_offset, const exprt &_value):
-    byte_update_exprt(ID_byte_update_little_endian, _op, _offset, _value)
-  {
-  }
-};
-
-inline const byte_update_little_endian_exprt
-  &to_byte_update_little_endian_expr(const exprt &expr)
-{
-  assert(expr.id()==ID_byte_update_little_endian && expr.operands().size()==3);
-  return static_cast<const byte_update_little_endian_exprt &>(expr);
-}
-
-inline byte_update_little_endian_exprt
-  &to_byte_update_little_endian_expr(exprt &expr)
-{
-  assert(expr.id()==ID_byte_update_little_endian && expr.operands().size()==3);
-  return static_cast<byte_update_little_endian_exprt &>(expr);
-}
-
-/*! \brief TO_BE_DOCUMENTED
-*/
-class byte_update_big_endian_exprt:public byte_update_exprt
-{
-public:
-  byte_update_big_endian_exprt():
-    byte_update_exprt(ID_byte_update_big_endian)
-  {
-  }
-
-  byte_update_big_endian_exprt(
-    const exprt &_op, const exprt &_offset, const exprt &_value):
-    byte_update_exprt(ID_byte_update_big_endian, _op, _offset, _value)
-  {
-  }
-};
-
-inline const byte_update_big_endian_exprt
-  &to_byte_update_big_endian_expr(const exprt &expr)
-{
-  assert(expr.id()==ID_byte_update_big_endian && expr.operands().size()==3);
-  return static_cast<const byte_update_big_endian_exprt &>(expr);
-}
-
-inline byte_update_big_endian_exprt
-  &to_byte_update_big_endian_expr(exprt &expr)
-{
-  assert(expr.id()==ID_byte_update_big_endian && expr.operands().size()==3);
-  return static_cast<byte_update_big_endian_exprt &>(expr);
 }
 
 #endif // CPROVER_UTIL_BYTE_OPERATORS_H
